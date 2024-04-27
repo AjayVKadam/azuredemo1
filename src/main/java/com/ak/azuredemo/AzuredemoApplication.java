c1c545108c04da12108c61748fdd556b04c4cf01
Ajay <ajaykadam@yahoo.com>
2024-04-27 21:55:26 +0530

    cleanup

--- src/main/java/com/ak/azuredemo/AzuredemoApplication.java
+++ src/main/java/com/ak/azuredemo/AzuredemoApplication.java
@@ -1,5 +1,6 @@
 package com.ak.azuredemo;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzuredemoApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
+
 		SpringApplication.run(AzuredemoApplication.class, args);
 	}
 
