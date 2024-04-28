4203abcfe735943e11b3c34bb2eeb3838648858d
Ajay <ajaykadam@yahoo.com>
2024-04-28 20:09:46 +0530

    app insights

--- src/main/java/com/ak/azuredemo/AzuredemoApplication.java
+++ src/main/java/com/ak/azuredemo/AzuredemoApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzuredemoApplication {
 
 	public static void main(String[] args) {
+		// enable Application Insights Java programmatically, see
+		// https://learn.microsoft.com/en-us/azure/azure-monitor/app/java-spring-boot#enabling-programmatically
+		https://github.com/Azure-Samples/spring-petclinic-java-mysql/tree/ec32fb0dc39b2c0c7026e8733a4df2a7784e6382
+
 		ApplicationInsights.attach();
 
 		SpringApplication.run(AzuredemoApplication.class, args);
